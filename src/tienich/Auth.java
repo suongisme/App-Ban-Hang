859596c3d107218b390300bc68ae411585b6c209
SuongNguyen <suongnguyenytm2001@gmail.com>
2020-11-19 12:11:53 +0700

    them lop tien ich Auth

---
+++ src/tienich/Auth.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienich;
+
+import entity.NhanVien;
+
+/**
+ *
+ * @author SuongNguyen
+ */
+public class Auth {
+    private static NhanVien user;
+    
+    public static boolean isManger() {
+        return "Quản lý".equalsIgnoreCase(user.getLoaiNhanVien());
+    }
+    
+    public static void logout() {
+        user = null;
+    }
+}
