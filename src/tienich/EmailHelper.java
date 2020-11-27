2d0ed2fdf729c512336c719804df6af537c04b1e
SuongNguyen <suongnguyenytm2001@gmail.com>
2020-11-27 17:26:54 +0700

    gui email bang text/html

--- src/tienich/EmailHelper.java
+++ src/tienich/EmailHelper.java
@@ -19,7 +19,8 @@ import javax.mail.internet.MimeMessage;
  *
  * @author SuongNguyen
  */
-public class EmailHelper extends Thread{
+public class EmailHelper extends Thread {
+
     private String destination;
     private String content;
     private String title;
@@ -28,17 +29,29 @@ public class EmailHelper extends Thread{
     private final String PASS_EMAIL = "12345678@a";
 
     private EmailHelper(String destination) {
-        codeReset = (int) (Math.random()*899999)+100000;
+        codeReset = (int) (Math.random() * 899999) + 100000;
         this.destination = destination;
         this.title = "[FaFo] Reset mật khẩu";
-        this.content = "Chúng tôi nghe nói rằng bạn bị mất mật khẩu trong FaFo.\n\n"
-                + "Đừng lo lắng nhé. Chúng tôi sẽ gửi cho bạn 1 đoạn mã để reset lại mật khẩu.\n\n"
-                + "Mã của bạn là: " + codeReset + "\n"
-                + "LƯU Ý: mã này chỉ tồn tại trong 1 phút.\n\n"
-                + "Thanks,\n"
-                + "FaFo Team.";
+        this.content = 
+                "<div style=\"width: 410px; height: 500px; display: block; margin: 0 auto;\">"
+                + "<p style=\"color: #3b5998; font-size: 19px;font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;\">"
+                + "   FastFood"
+                + "</p>"
+                + "<p style=\"font-size:20px;font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;\"> "
+                + "   Hi " + Auth.user.getTenNhanVien() + ","
+                + "   <br><br> "
+                + "   Chúng tôi nhận được yêu cầu khôi phục mật khẩu của bạn. <br> "
+                + "   Nhập đoạn code phía dưới để khôi phục mật khẩu: "
+                + "</p> "
+                + "<p style=\"text-align: center;font-size:25px;font-family:LucidaGrande,tahoma,verdana,arial,sans-serif;padding:10px;background-color:#f2f2f2;font-weight: bold;\"> " + 
+                      codeReset + ""
+                + "</p> "
+                + "<b>*Lưu ý: code này chỉ tồn tại trong vòng 1 phút.</b> "
+                + "<p>Thanks,<br>FaFo Team.</p>"
+                + "</div>";
+
     }
-    
+
     private Authenticator getAuthenticator() {
         return new Authenticator() {
             @Override
@@ -47,7 +60,7 @@ public class EmailHelper extends Thread{
             }
         };
     }
-    
+
     private Properties getProperties() {
         Properties pro = new Properties();
         pro.put("mail.smtp.host", "smtp.gmail.com");
@@ -56,21 +69,21 @@ public class EmailHelper extends Thread{
         pro.put("mail.smtp.port", 587);
         return pro;
     }
-    
+
     private Session getSession() {
         return Session.getInstance(this.getProperties(), this.getAuthenticator());
     }
-    
+
     private Message getMessage() throws MessagingException {
         Message mess = new MimeMessage(this.getSession());
         mess.setSubject("[FaFo] Lấy lại mật khẩu");
         mess.setReplyTo(InternetAddress.parse(destination));
         mess.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destination));
-        mess.setText(content);
+        mess.setContent(content, "text/html; charset=utf-8");
         return mess;
     }
-    
-    public static void sendEmail(String destination){
+
+    public static void sendEmail(String destination) {
         EmailHelper em = new EmailHelper(destination);
         em.start();
     }
