8cc1a8c92adca74636a8835f76d722eb3d7f4411
SuongNguyen <suongnguyenytm2001@gmail.com>
2020-11-28 15:08:06 +0700

    sua lai ten nguoi gui trong EmailHelper

--- src/tienich/EmailHelper.java
+++ src/tienich/EmailHelper.java
@@ -5,7 +5,10 @@
  */
 package tienich;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -32,22 +35,25 @@ public class EmailHelper extends Thread {
         codeReset = (int) (Math.random() * 899999) + 100000;
         this.destination = destination;
         this.title = "[FaFo] Reset mật khẩu";
-        this.content = 
+        this.content =
                 "<div style=\"width: 410px; height: 500px; display: block; margin: 0 auto;\">"
-                + "<p style=\"color: #3b5998; font-size: 19px;font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;\">"
-                + "   FastFood"
-                + "</p>"
-                + "<p style=\"font-size:20px;font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;\"> "
-                + "   Hi " + Auth.user.getTenNhanVien() + ","
-                + "   <br><br> "
-                + "   Chúng tôi nhận được yêu cầu khôi phục mật khẩu của bạn. <br> "
-                + "   Nhập đoạn code phía dưới để khôi phục mật khẩu: "
-                + "</p> "
-                + "<p style=\"text-align: center;font-size:25px;font-family:LucidaGrande,tahoma,verdana,arial,sans-serif;padding:10px;background-color:#f2f2f2;font-weight: bold;\"> " + 
-                      codeReset + ""
-                + "</p> "
-                + "<b>*Lưu ý: code này chỉ tồn tại trong vòng 1 phút.</b> "
-                + "<p>Thanks,<br>FaFo Team.</p>"
+                + "  <div style=\"height:50px; border-bottom: 1px solid #A9A9A9\">"
+                + "    <img style=\"width:50px; height:50px; float: left\" src=\"https://st.depositphotos.com/3079665/3991/v/950/depositphotos_39915779-stock-illustration-chef-giving-thumbs-up.jpg\">"
+                + "    <p style=\"float:left;color: #3b5998; font-size: 19px;font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;\">"
+                + "        FastFood"
+                + "    </p>"
+                + "  </div>"
+                + "  <p style=\"font-size:20px;font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;\">"
+                + "      Hi " +Auth.user.getTenNhanVien()
+                + "      <br><br>"
+                + "      Chúng tôi nhận được yêu cầu khôi phục mật khẩu của bạn. <br>"
+                + "      Nhập đoạn code phía dưới để khôi phục mật khẩu:"
+                + "  </p>"
+                + "  <p style=\"text-align: center;font-size:25px;font-family:LucidaGrande,tahoma,verdana,arial,sans-serif;padding:10px;background-color:#f2f2f2;font-weight: bold;\"> "
+                + "        "+codeReset
+                + "  </p>"
+                + "  <b style=\"font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;\">*Lưu ý: code này chỉ tồn tại trong vòng 1 phút.</b>"
+                + "  <p>Thanks, <br>FaFo team.</p> "
                 + "</div>";
 
     }
@@ -74,8 +80,9 @@ public class EmailHelper extends Thread {
         return Session.getInstance(this.getProperties(), this.getAuthenticator());
     }
 
-    private Message getMessage() throws MessagingException {
+    private Message getMessage() throws MessagingException, UnsupportedEncodingException {
         Message mess = new MimeMessage(this.getSession());
+        mess.setFrom(new InternetAddress(EMAIL_ADDRESS, "Fast Food"));
         mess.setSubject("[FaFo] Lấy lại mật khẩu");
         mess.setReplyTo(InternetAddress.parse(destination));
         mess.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destination));
@@ -92,8 +99,9 @@ public class EmailHelper extends Thread {
     public void run() {
         try {
             Transport.send(this.getMessage());
-        } catch (MessagingException ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+            MsgBox.notify("Gửi email lỗi", null);
         }
     }
 }
